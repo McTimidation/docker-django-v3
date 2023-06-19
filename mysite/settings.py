9d0954b86f9d8b53c3eafc86b0db7b8446108fef
Michael McGraw <michaelmcgraw09@gmail.com>
2023-06-19 12:14:03 -0400

    adjusted allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-13gv8-bj@1mn+4a!ehpbsa9#v+*5a27n3wu+adwe49@o)=nemc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
